bf4ba72be5a5b01dccc161754930293420585513
Vicaious <1020681055@qq.com>
2019-07-04 10:12:18 +0800

    changhao

---
+++ DBHelper/DBhelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper
+{
+    public class DBhelper
+    {
+        public DataTable Cmd_Select(string connectstring,string sqlcmd)
+        {
+            DataTable table = new DataTable();
+            SqlConnection con = new SqlConnection(connectstring);
+            try
+            {               
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = sqlcmd;
+                cmd.CommandType = CommandType.Text;
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    da.Fill(table);
+                }
+                return table;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("DBHelper_Cmd_Select异常", e);
+            }
+            finally
+            {
+                con.Close();
+            }           
+        }
+
+        public bool Cmd_Commit(string connectstring, string sqlcmd)
+        {
+            SqlConnection con = new SqlConnection(connectstring);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = sqlcmd;
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter MyDataAdapter = new SqlDataAdapter(cmd);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("DBHelper_Cmd_Commit异常", e);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
